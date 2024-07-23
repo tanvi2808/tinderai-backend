a3f9921918e44f54e6ddc4a4350c3cddb616a61c
tanvi2808 <tanvi2808@gmail.com>
2024-07-23 21:20:50 +0530

    Basic working structure with all the dependencies and openai api key defined in env variable

---
+++ tinderai-backend/src/main/java/com/springframework/tinderai_backend/TinderaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.tinderai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderaiBackendApplication.class, args);
+	}
+
+}
---
+++ tinderai-backend/src/test/java/com/springframework/tinderai_backend/TinderaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.tinderai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
